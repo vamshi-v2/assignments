8895dc112b0859d8d34e2d92f0c4a8f0ea068933
vamshi-v2 <vamshi.prasad@v2solutions.com>
2025-11-25 11:47:50 +0530

    Fixing jest

--- testing/scr/pattern1.js
+++ testing/scr/pattern1.js
@@ -1,19 +1,22 @@
 function pattern1(n) {
+  
   let line = '';
-
   if (n < 0) {
     throw new Error("Input should be positive");
   }
-
+  
   else {
     for (let i = 1; i <= n; i++) {
       for (let s = 1; s <= n - i; s++) {
         line += ' ';
       }
 
-      for (let j = 1; j <= 2 * i - 1; j++) {
+      function numb(w){for (let j = 1; j <= 2 * w - 1; j++) {
         line += 1;
+        // console.log(line);
       }
+    }
+    numb(i);
       if (i !== n) {
         line += "\n";
       }
@@ -22,6 +25,6 @@ function pattern1(n) {
   return line;
 }
 
-// console.log(pattern1(5));
+console.log(pattern1(5));
 
 module.exports = pattern1;
--- testing/tests/pattern1.test.js
+++ testing/tests/pattern1.test.js
@@ -9,14 +9,20 @@ const pattern1 = require('../scr/pattern1');
 111111111`
 expect(pattern1(5)).toBe(exp)}
   );
-
-
   test('test case 2 for pattern1 :', () => {
 expect(() => pattern1(-3)).toThrow("Input should be positive");
  });
 
- test("test case 3 for patterb1 (100 lines) :",()=>{
-    const lines=100;
+ test("test case 3 for pattern1 (100 lines) :",()=>{
+    const lines=3;
+    const patt=pattern1(lines);
+    let char= ((3/2)*lines*lines)+((1/2)*lines)-1;
+    expect(patt.length).toBe(char);
+
+ });
+
+  test("test case 4 for pattern1 (n lines) :",()=>{
+    const lines=3;
     const patt=pattern1(lines);
     let char= ((3/2)*lines*lines)+((1/2)*lines)-1;
     expect(patt.length).toBe(char);
